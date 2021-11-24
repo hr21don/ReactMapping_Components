e1cca1167a344c69625a636dc4f236cc3f7e0f78
Helitha Rupasinghe <91548582+hr21don@users.noreply.github.com>
2021-11-24 20:51:02 +0000

    update to readme and app.jsx

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # ReactMapping_Components
+
 {React Development Journey PT 4}
 
 ## Output
-Here what the example output should look like.....|| Get in touch if you have trouble launching the app!
 
+Here what the example output should look like.....|| Get in touch if you have trouble launching the app!
 
 <img width="883" alt="capture" src="https://user-images.githubusercontent.com/91548582/143305841-8aa13caa-0736-4f9a-8d0f-97b73ce4fd02.PNG">
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,22 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
 
-function createEntry(emoji_Col) {
-  return (
-    <Entry
-      key={emoji_Col.id}
-      emoji={emoji_Col.emoji}
-      name={emoji_Col.name}
-      description={emoji_Col.description}
-    />
-  );
-}
 function App() {
   return (
     <div>
       <h1>
         <span>emojipedia</span>
       </h1>
-      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+      <dl className="dictionary">
+        {emojipedia.map((emoji_Col) => (
+          <Entry
+            key={emoji_Col.id}
+            emoji={emoji_Col.emoji}
+            name={emoji_Col.name}
+            description={emoji_Col.description}
+          />
+        ))}
+      </dl>
     </div>
   );
 }
